ec8820ad7a25a794d97077309d16d10c5579324d
JavSanchez97 <javsanchez2015@gmail.com>
2022-03-13 16:22:45 -0400

    Created operate function

--- script.js
+++ script.js
@@ -3,22 +3,37 @@
 
 
 
+function operate(operator, a, b){
+    a = Number(a);
+    b = Number(b);
+    switch(operator){
+        case'+':
+            return (add(a,b));
+        case'-':
+            return(subtract(a,b));
+        case'*':
+            return(multiply(a,b));
+        case'÷':
+            return(divide(a,b));
+        default:
+            return null;
+    }
+}
+
 /* 
 These are the four main basic math functions available
  */
-// takes two variables, adds, returns sum
+
+// returns sum, difference, product, or quotient
 function add(a, b){
     return a + b;
 }
-//returns difference
 function subtract(a, b){
     return a - b;
 }
-//returns product
 function multiply(a,b){
     return a * b;
 }
-//returns quotient, also has a case for divide by 0
 function divide(a,b){
     if(b == 0){return "Error! You shouldn't have done that!"}
     else{
